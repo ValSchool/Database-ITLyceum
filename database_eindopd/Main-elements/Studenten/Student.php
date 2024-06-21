7e740b31e7ed175f40ceed8668dfdc0fbd2bc0be
Martinosimic <151631870+Martinosimic@users.noreply.github.com>
2024-06-21 12:15:10 +0200

    studenten

--- database_eindopd/Main-elements/Studenten/Student.php
+++ database_eindopd/Main-elements/Studenten/Student.php
@@ -3,15 +3,15 @@
 class Student {
     private $db;
 
-    public function insertStudent($Student_id, $email, $password_hash) {
+    public function insertStudent($student_id, $klas_id, $naam, $achternaam, $email, $password_hash) {
        
         $hashedPassword = password_hash($password_hash, PASSWORD_DEFAULT);
     
-        return $this->db->exec("INSERT INTO studenten (naam, email, password_hash) VALUES (?, ?, ?, ?)", [$naam, $email, $hashedPassword]);
+        return $this->db->exec("INSERT INTO studenten (student_id, klas_id, naam, achternaam, email, password_hash) VALUES (?, ?, ?, ?, ?, ?)", [$student_id, $klas_id, $naam, $achternaam, $email, $password_hash]);
     }
 
     public function selectStudent() {
-        return $this->db->exec("SELECT * from Studenten WHERE Student_id = ?");
+        return $this->db->exec("SELECT * from Studenten WHERE student_id = ?");
     }
     
 }
