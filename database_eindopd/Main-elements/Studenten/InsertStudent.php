4b452e54b48a379fde30744bc218641069d9bc4f
Martinosimic <151631870+Martinosimic@users.noreply.github.com>
2024-06-21 12:06:51 +0200

    changed files

--- database_eindopd/Main-elements/Studenten/InsertStudent.php
+++ database_eindopd/Main-elements/Studenten/InsertStudent.php
@@ -5,7 +5,5 @@ require_once 'Student.php';
 require_once '../../Db_connection.php';
 
 $myDb = new DB('itlyceum');
-$docent = new Docent($myDb);
+$student = new Student($myDb);
 
-
-?>
--- database_eindopd/Main-elements/Studenten/Student.php
+++ database_eindopd/Main-elements/Studenten/Student.php
@@ -3,11 +3,15 @@
 class Student {
     private $db;
 
-    public function insertDocent($naam, $email, $password) {
+    public function insertStudent($Student_id, $email, $password_hash) {
        
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $hashedPassword = password_hash($password_hash, PASSWORD_DEFAULT);
     
         return $this->db->exec("INSERT INTO studenten (naam, email, password_hash) VALUES (?, ?, ?, ?)", [$naam, $email, $hashedPassword]);
+    }
+
+    public function selectStudent() {
+        return $this->db->exec("SELECT * from Studenten WHERE Student_id = ?");
     }
     
 }
